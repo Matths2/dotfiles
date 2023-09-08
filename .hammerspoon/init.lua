98274c6cdbbc94152c8d5af23d9bc3e044000cc8
Mattias Holmgren <mattias.holmgren@tietoevry.com>
2023-09-08 22:30:18 +0200

    reversed symlinks to include hammerspoon scripts

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,1 +1,1 @@
-require('keyboard') -- Load Hammerspoon bits from https://github.com/jasonrudolph/keyboard
+require("hammerspoon") -- Load Hammerspoon bits from https://github.com/jasonrudolph/keyboard
